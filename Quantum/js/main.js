b13e6a9f797b2bbc9207b32e7108be8841dc10a9
tanmaypandit2003 <tanmaypandit2003@gmail.com>
2024-07-05 21:15:11 +0530

    Added Quantum

---
+++ Quantum/js/main.js
@@ -0,0 +1,20 @@
+// Header Scroll
+
+let nav= document.querySelector(".navbar"); 
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// how to hide nav when it scroll
+
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})  
